c92920079ade697fb6dd3cd68e77ac3b040cf23b
Ireneusz Luczak <ericluczak@Ireneuszs-MacBook-Pro.local>
2015-08-05 16:10:23 -0500

    zomg a working database such amaze much proud

--- controllers/UploadController.rb
+++ controllers/UploadController.rb
@@ -8,47 +8,65 @@ class UploadController < ApplicationController
     names_array = heroes_doc.xpath("//div[@class='name']/text()")
     puts names_array
 
-
     names_array.each() do |hero|
-      loop_counter = 1
-      puts loop_counter
       @hcountermodel = HeroCountersModel.new
       @hcountermodel.name = hero
-
       uri_hero = hero.to_s.downcase.gsub(" ","-").gsub("'","")
+
+      puts "----"
+      puts uri_hero
+      puts "----"
 
       hero_img_doc = Nokogiri::HTML(open("http://www.dotabuff.com/heroes/#{uri_hero}"))
       img = hero_img_doc.xpath("//img[@class='image-avatar image-hero']/@src").to_s
-      puts img
       img.slice!(0)
-      puts img
       image_base = "http://dotabuff.com/"
       uri_name = image_base + img
-      puts uri_name
       img = 'public/images/' + img
-      puts img
 
       @hcountermodel.imageurl = img
-      puts uri_name
       File.open(img, 'wb') do |fo|
         fo.write(open(uri_name).read)
       end
-      puts 'we got this far'
-      @hcountermodel.save
-      loop_counter = loop_counter + 1
-      end
+
+#--------------------------------------------
+# Everything above this line works. that code finds all the names and images. Throws the names in the database, populates public folder with images, and throws image path into db. Don't mess...
+#--------------------------------------------
+
+
+
+    hero_counter_doc = Nokogiri::HTML(open("http://www.dotabuff.com/heroes/#{uri_hero}"))
+    hero_counter_array = hero_counter_doc.xpath("//section[5]/article/table/tbody/tr/td[2]/a[@class='link-type-hero']/text()")
+    count_int = 1
+    hero_counter_array.each() do |item|
+      count_index = 'counter' + count_int.to_s + '='
+      @hcountermodel.send(count_index, item)
+      count_int = count_int + 1
+    end
+    # Add the counter image from 1 to 8 for the hero
+    hero_counter_img_doc = Nokogiri::HTML(open("http://www.dotabuff.com/heroes/#{uri_hero}"))
+    hero_counter_img_array = hero_counter_img_doc.xpath("//section[5]/article/table/tbody/tr/td[@class='cell-icon']/div[@class='image-container image-container-icon image-container-hero']/a/img[@class='image-icon image-hero']/@src")
+    countimg_int = 1
+    hero_counter_img_array.each() do |item|
+      item = "public/images" + item
+      puts item
+      countimg_index = "counterimageurl" + countimg_int.to_s + '='
+      @hcountermodel.send(countimg_index, item)
+      countimg_int = countimg_int + 1
+    end
 
 
 
 
 
 
+        @hcountermodel.save
+      end # end of loop
+
+    end #end of function
 
 
 
-
-
-    end #End of function
 
 
 
