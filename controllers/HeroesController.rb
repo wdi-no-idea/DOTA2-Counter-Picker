99131e904dd5407d1448882bc85ea54b6247fa75
Ireneusz Luczak <ericluczak@Ireneuszs-MacBook-Pro.local>
2015-08-05 17:04:19 -0500

    its 5 oclock

--- controllers/HeroesController.rb
+++ controllers/HeroesController.rb
@@ -1,11 +1,14 @@
 class HeroesController < ApplicationController
 
   get '/' do
+
+    @heroes = HeroCountersModel.all
+
     erb :'heroes_views/heroes_index'
   end
 
   post '/' do
     erb :'heroes_views/heroes_index'
   end
-  
+
 end
